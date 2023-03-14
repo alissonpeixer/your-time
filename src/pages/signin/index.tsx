6908fa139984fb93fcd5ce5c0734507a0bb68cf4
alissonpeixer <alissonpeixer4@gmail.com>
2023-03-13 22:44:59 -0300

    feat: created router signin

---
+++ src/pages/signin/index.tsx
@@ -0,0 +1,13 @@
+import { Container } from "@/components/molecules/Container"
+
+
+const Signin = () => {
+  return(
+    <Container title="Sign In">
+      <h1>Olá mundo </h1>
+    </Container>
+  )
+}
+
+
+export default Signin
